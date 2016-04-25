94c22a431f814d1698229aaba3f10f14fb10fee0
Marcio Klepacz <marcioklepacz@gmail.com>
2016-04-25 22:51:16 +0200

    Add second experiment: trying to return something

--- CWrapper/Wrapper.c
+++ CWrapper/Wrapper.c
@@ -1,15 +1,12 @@
 #include <ruby.h>
 
-void call_function(const char *function_name, const char *file_name) {
+uintptr_t call_function(const char *function_name, const char *file_name) {
 
   if (ruby_setup()) {
 		fprintf(stderr, "Failed to init Ruby VM\n");
 	}
 
   rb_require(file_name);
-  rb_funcall(rb_mKernel, rb_intern(function_name), 0, NULL);
-
-	ruby_cleanup(0);
-
+  return rb_funcall(rb_mKernel, rb_intern(function_name), 0, NULL);
 }
 
--- CWrapper/Wrapper.h
+++ CWrapper/Wrapper.h
@@ -1,1 +1,1 @@
-void call_function(const char *function_name, const char *file_name);
+uintptr_t call_function(const char *function_name, const char *file_name);
